9b3709ad9721e92d21b9fbec99394f13d73a9510
lauret sydney <lrs3266a@ens.fsi>
2022-10-07 09:41:50 +0200

    TP1 methode toString

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,10 +3,30 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
-	
-	public void recevoirCoup(int i) {
-		// TODO Auto-generated method stub
 
+	public Romain(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
 	}
 
+	public String getNom() {
+		return nom;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+
+	private String prendreParole() {
+		return "Le romain " + nom + " : ";
+	}
+
+	public void recevoirCoup(int forceCoup) {
+		force -= forceCoup;
+		if (force > 0) {
+			parler("Aïe");
+		} else {
+			parler("J'abandonne...");
+		}
+	}
 }
