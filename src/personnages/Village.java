058c4c58492288eff92ae86cc3e70c8a36d813b4
Sydney Lauret <sydney.lauret@gmail.com>
2022-10-17 23:35:31 +0200

    Le village gaulois

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -6,8 +6,9 @@ public class Village {
 	private Gaulois[] villageois;
 	private int nbVillageois = 0;
 
-	public Village(String nom) {
+	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
+		this.villageois = new Gaulois[nbVillageoisMaximum];
 	}
 
 	public void setChef(Chef chef) {
@@ -16,5 +17,45 @@ public class Village {
 
 	public String getNom() {
 		return nom;
+	}
+	
+	public void ajouterHabitant(Gaulois gaulois) {
+		villageois[nbVillageois] = gaulois;
+		nbVillageois += 1;
+	}
+	
+	public Gaulois trouverHabitant(int i) {
+		return villageois[i];
+	}
+	
+	public void afficherVillageois() {
+		System.out.println("Dans le village du chef " + chef.getNom() + " vivent les légendaires gaulois :");
+		int i = 0;
+		do {
+			System.out.println("- " + villageois[i].getNom());
+			i += 1;
+		} while ((villageois[i] != null) && (i < 30));
+	}
+	
+	public static void main(String[] args) {
+		Village village = new Village("Village des Irréductibles", 30);
+		
+//		Gaulois gaulois = village.trouverHabitant(30);
+//
+//		Le dernier indice du tableau est 29.
+		
+		Chef abraracourcix = new Chef("Abraracourcix", 6, village);
+		village.setChef(abraracourcix);
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		village.ajouterHabitant(asterix);
+		
+//		Gaulois gaulois = village.trouverHabitant(1);
+//		System.out.println(gaulois);
+//
+//		Astérix est positionné à l'indice 0.
+		
+		Gaulois obelix = new Gaulois("Obélix", 25);
+		village.ajouterHabitant(obelix);
+		village.afficherVillageois();
 	}
 }
