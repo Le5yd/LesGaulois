2b2b508fd4f138e3f28472e54219cd01beaeea8b
Sydney Lauret <sydney.lauret@gmail.com>
2022-10-17 18:07:37 +0200

    TP1 methode toString

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,24 +3,24 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
-	
+
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
 	}
-	
+
 	public String getNom() {
 		return nom;
 	}
-	
+
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "« " + texte + "»");
 	}
-	
+
 	private String prendreParole() {
 		return "Le romain " + nom + " : ";
 	}
-	
+
 	public void recevoirCoup(int forceCoup) {
 		force -= forceCoup;
 		if (force > 0) {
@@ -28,5 +28,12 @@ public class Romain {
 		} else {
 			parler("J'abandonne...");
 		}
+	}
+	
+	public static void main(String[] args) {
+		Romain caiusBonus = new Romain("Caius Bonus", 10);
+		caiusBonus.parler("Ave, César !");
+		caiusBonus.recevoirCoup(1);
+		caiusBonus.recevoirCoup(30);
 	}
 }
